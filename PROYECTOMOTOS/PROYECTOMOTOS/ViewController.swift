fe2c1f39bced3a63df8f5ab9f646684eb132ac5e
Prog <prog@iMac-EPH-F-07.local>
2019-03-12 21:41:40 -0500

    Version 1.0

---
+++ PROYECTOMOTOS/PROYECTOMOTOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PROYECTOMOTOS
+//
+//  Created by Prog on 2/26/19.
+//  Copyright © 2019 OADIAZC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
